4d3fdc6ae7d1e209f0f6f913c5f388ed87bec74c
张昌昌 <zhcc@zving.com>
2018-09-28 19:00:36 +0800

    添加文件

---
+++ readme.md
@@ -0,0 +1,8 @@
+# MJ_ExportFile 文件导出说明
+
+***本项目copy郭亮项目，github地址为***
+```
+https://github.com/sinmahod/exportFile
+```
+
+> 本项目为Eclipse插件，导出编译好的class到指定目录下
---
+++ src/com/mj/exportfile/util/DirList.java
@@ -0,0 +1,41 @@
+package com.mj.exportfile.util;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Comparator;
+import java.util.regex.Pattern;
+
+/**
+ * @addtime 2015-07-03 13:31:31
+ * @author gl
+ */
+public class DirList {
+	public static String[] getFile(String dir , String regex) {
+		File path = new File(dir);
+		String[] list = path.list(new DirFilter(regex));
+		return list;
+	}
+}
+
+class DirFilter implements FilenameFilter {
+	private Pattern pattern;
+
+	public DirFilter(String regex) {
+		pattern = Pattern.compile(regex);
+	}
+
+	@Override
+	public boolean accept(File dir, String name) {
+		return pattern.matcher(new File(name).getName()).matches();
+	}
+} 
+
+@SuppressWarnings("rawtypes")
+class AlphabeticComparator implements Comparator {
+	@Override
+	public int compare(Object o1, Object o2) {
+		String s1 = (String) o1;
+		String s2 = (String) o2;
+		return s1.toLowerCase().compareTo(s2.toLowerCase());
+	}
+}
